b3f306e311527621b54b1067ef231555d38f4db7
Виталий Коваленко <vitalijkovalenko@MacBook-Pro-Vitalij.local>
2023-05-18 21:43:24 +0300

    ready

--- src/PathWay.jsx
+++ src/PathWay.jsx
@@ -5,6 +5,7 @@ import Main from './components/Main/Main';
 import Catalog from './pages/Catalog/Catalog';
 // import Product from './pages/Product/Product';
 import Footer from './components/Footer/Footer';
+import OpenCard from './components/OpenCard/OpenCard';
 // import NotFound from './pages/NotFound/NotFound';
 
 const PathWay = ()=> {
@@ -17,6 +18,7 @@ const PathWay = ()=> {
       <Routes>
         <Route exact path="/" element={<Main/>} />
         <Route exact path="/catalog" element={<Catalog selected={selected} setSelected={setSelected}/>} />
+        <Route exact path="/openCard" element={<OpenCard/>} />
         {/* <Route path="/catalog/:productId" component={Product} />
         <Route component={NotFound} /> */}
       </Routes>
---
+++ src/components/OpenCard/OpenCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Slider from '../CarouselFadeExample/CarouselFadeExample'
+
+const OpenCard = () => {
+  return (
+    <div className='container'>
+        <div className='open-card-items'>
+            <div className='open-card-img'>
+                <Slider/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default OpenCard
